49e4eb0ca8c55e222993c0de942795a927cc17eb
Michał Poręba <michalporeba@gmail.com>
2024-04-07 13:48:37 +0100

    checklist basic model

--- packages/ticks_api/lib/src/model/checklist.dart
+++ packages/ticks_api/lib/src/model/checklist.dart
@@ -1,3 +1,39 @@
-class Checklist {
-  
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+import 'package:ticks_api/src/model/json_map.dart';
+
+part 'checklist.g.dart';
+
+@immutable
+@JsonSerializable()
+class Checklist extends Equatable {
+  /// {@macro checklist}
+  const Checklist({
+    required this.blueprintId,
+    required this.title,
+    this.id = '',
+  });
+
+  final String id;
+  final String blueprintId;
+  final String title;
+
+  Checklist copyWith({
+    String? id,
+    String? blueprintId,
+    String? title,
+  }) {
+    return Checklist(
+      id: id ?? this.id,
+      blueprintId: blueprintId ?? this.blueprintId,
+      title: title ?? this.title,
+    );
+  }
+
+  static Checklist fromJson(JsonMap json) => _$ChecklistFromJson(json); 
+  JsonMap toJson() => _$ChecklistToJson(this);
+
+  @override
+  List<Object> get props => [id, blueprintId, title];
 }
--- packages/ticks_api/test/src/ticks_api_test.dart
+++ packages/ticks_api/test/src/ticks_api_test.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:test/test.dart';
 import 'package:ticks_api/src/model/blueprint.dart';
+import 'package:ticks_api/src/model/checklist.dart';
 
 void main() {
   group('Blueprint', () {
     test('can be instantiated', () {
       expect(Blueprint(title: "test"), isNotNull);
+    });
+  });
+  group('Checklist', () {
+    test('can be instantiated', () {
+      const blueprint = Blueprint(title: "test");
+      expect(Checklist(blueprintId: blueprint.id, title: "test"), isNotNull);
     });
   });
 }
