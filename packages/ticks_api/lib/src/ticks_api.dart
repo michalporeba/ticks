7471f51d6dfd9f298d16c0920b4b2de1e7e1661f
Michał Poręba <michalporeba@gmail.com>
2024-04-07 12:13:37 +0100

    adding minimum api interface

--- packages/ticks_api/lib/src/ticks_api.dart
+++ packages/ticks_api/lib/src/ticks_api.dart
@@ -1,7 +1,15 @@
+import 'package:ticks_api/src/model/blueprint.dart';
+
+import 'model/checklist.dart';
+
 /// {@template ticks_api}
 /// The interfaces and model for the API providing access to ticks
 /// {@endtemplate}
-class TicksApi {
+abstract class TicksApi {
   /// {@macro ticks_api}
   const TicksApi();
+
+  Stream<List<Checklist>> getActiveChecklists();
+
+  Stream<List<Blueprint>> getBlueprints();
 }
