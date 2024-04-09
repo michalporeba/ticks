ed275f8decfa86cd7470b66041cc80d330644e6b
Michał Poręba <michalporeba@gmail.com>
2024-04-09 07:55:26 +0100

    resizing the icons

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  static double scaleFactor = 1.5;
+  static double defaultIconSize = 24;
+
   static ThemeData sjac(BuildContext context) {
+    final base = ThemeData.light();
+
     return ThemeData(
       useMaterial3: true,
+      appBarTheme: base.appBarTheme.copyWith(
+        iconTheme: IconThemeData(size: defaultIconSize * scaleFactor)
+      ),
       colorScheme: const ColorScheme(
           brightness: Brightness.light,
           background: Color(0xfffff7ab),
@@ -19,8 +27,14 @@ class AppTheme {
           onSecondary: Color(0xffffffff),
 
       ),
+      iconTheme: base.iconTheme.copyWith(
+        size: defaultIconSize * scaleFactor,
+      ),
+      primaryIconTheme: base.iconTheme.copyWith(
+        size: defaultIconSize * scaleFactor,
+      ),
       textTheme: GoogleFonts.heeboTextTheme(
-        Theme.of(context).textTheme.apply(fontSizeFactor: 1.5),
+        Theme.of(context).textTheme.apply(fontSizeFactor: scaleFactor),
       ),
     );
   }
